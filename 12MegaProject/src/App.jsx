d5e7641a30d092de7167c355bd3acecb239537e5
QaisarMoin <qaisarashraf685@gmail.com>
2024-02-16 00:15:17 +0530

    with some more

--- 12MegaProject/src/App.jsx
+++ 12MegaProject/src/App.jsx
@@ -1,13 +1,40 @@
-import { useState, useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authSerivce from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 import "./App.css";
+import { Header, Footer } from "./components";
 
 function App() {
-  return (
-    <>
-      <h1>Hello i am Qaisar Moin</h1>
-    </>
-  );
+  const [loader, setloader] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    // ye function sirf check kar raha hai ke user login hai ke nahi bas
+    authSerivce
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setloader(false));
+  }, []);
+
+  return !loader ? (
+    <div
+      className="min-h-screen flex flex-wrap
+     content-between bg-gray-400
+    "
+    >
+      <div className="w-full block">
+        <Header />
+        Hello
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
--- 12MegaProject/src/store/authSlice.js
+++ 12MegaProject/src/store/authSlice.js
@@ -22,4 +22,5 @@ export const authSlice = createSlice({
   },
 });
 
+export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
